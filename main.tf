7c0c9c88a4ac602af0b6dc16de7220de702e7d08
Troy Larson <troylars@147dda2fd3d0.ant.amazon.com>
2021-03-20 20:19:28 -0400

    Fix org name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "blue-acorn-dev"
+    organization = "Snaptech"
 
     workspaces {
       name = "blue-acorn-dev"
